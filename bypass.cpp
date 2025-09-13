b2b1f5d20c0769d3cf2c5e36a6e869bd71ee1c6b
seffocx <wleqx0@gmail.com>
2025-09-13 19:28:17 +0300

    Create bypass.cpp

---
+++ bypass.cpp
@@ -0,0 +1,13 @@
+PATCH_LIB("libanogs.so","0x322140","00 00 80 D2 C0 03 5F D6");
+PATCH_LIB("libanogs.so","0x47BEA8","00 00 80 D2 C0 03 5F D6");
+PATCH_LIB("libanogs.so","0x48D3C4","00 00 80 D2 C0 03 5F D6");
+PATCH_LIB("libanogs.so","0x4980AC","00 00 80 D2 C0 03 5F D6");
+PATCH_LIB("libanogs.so","0x4980AC","00 00 80 D2 C0 03 5F D6");
+PATCH_LIB("libanogs.so","0x1D218C","00 00 80 D2 C0 03 5F D6");
+PATCH_LIB("libanogs.so","0x1CDA20","00 00 80 D2 C0 03 5F D6");
+PATCH_LIB("libanogs.so","0x384CF0","00 00 80 D2 C0 03 5F D6");
+PATCH_LIB("libanogs.so","0x4F1838","00 00 80 D2 C0 03 5F D6");
+PATCH_LIB("libanogs.so","0x47C274","00 00 80 D2 C0 03 5F D6");
+PATCH_LIB("libanogs.so","0x443274","00 00 80 D2 C0 03 5F D6");
+PATCH_LIB("libanogs.so","0x442C78","00 00 80 D2 C0 03 5F D6");
+PATCH_LIB("libUE4.so", "0x7334BA8", "00 00 80 D2 C0 03 5F D6");//SendClientLog
